9b29a61b6fc06ec2d9c74c1f0c4eb8ad1ef2fe19
tsraveling <tsraveling@gmail.com>
2019-06-18 14:36:48 -0600

    Stubbed out parser to separate parsing from dynamic stuff

--- example/src/library/skald.js
+++ example/src/library/skald.js
@@ -1,19 +1,30 @@
 export default class Skald {
 
-    constructor(skaContents) {
-        this.skaContents = skaContents;
-        console.log("Loaded file: \n\n");
-        console.log(this.skaContents);
+    constructor(skaObject) {
+        this.skaObject = skaObject;
+    }
 
+    static parseSkaText(skaText) {
+
+        // TODO: Parse the long file string into the JSON object format we will use to run the actual processing
+
+        return {
+            exampleValue: "This is an example value"
+        };
     }
 
     static async buildDynamically(skaFile) {
 
+        // Get the contents of the file
         let response = await fetch(skaFile);
         let textData = await response.text();
 
-        let skaObject = new Skald(textData);
-        return skaObject;
+        // Parse the contents into a useable Skald Object:
+        let skaObject = Skald.parseSkaText(textData);
+
+        // Generate the Skald instance using the object, and return it
+        // Note: in production, you'll want to do this directly from a json file.
+        return new Skald(skaObject);
     }
 
     perform(func, state) {
@@ -21,6 +32,6 @@ export default class Skald {
         console.log("Perform call:")
 
         // For now just print the file
-        return this.skaContents;
+        return this.skaObject.exampleValue;
     }
 }
