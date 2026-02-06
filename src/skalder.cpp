0898be443c4aaf31bfef47e803a21129aac19194
Tim Raveling <tsraveling@proton.me>
2026-02-05 17:36:38 -0700

    fix: started centralizing into tester class

--- src/skalder.cpp
+++ src/skalder.cpp
@@ -1,6 +1,5 @@
 #include "debug.h"
 #include "skald.h"
-#include "skald_grammar.h"
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
@@ -12,10 +11,89 @@
 using namespace ftxui;
 using namespace Skald;
 
+enum class NarrativeItemType { NORMAL, SYSTEM, ERROR };
+
+struct NarrativeItem {
+  std::string attribution;
+  std::string content;
+  NarrativeItemType type = NarrativeItemType::NORMAL;
+};
+
 class SkaldTester {
 public:
+  /** List of content; latest is current. */
+  std::vector<NarrativeItem> narrative;
+
+  /** The current response we are going to be responding to */
+  Response &current_response;
+
+  enum InputType { CONTINUE, CHOICES, TEXT };
+  std::string current_prompt;
+  std::vector<std::string> current_options;
+  InputType expected_input;
+
+  /** Gets the input type expected for a given response */
+  // STUB: Delete me, obsoleted below
+  InputType input_type_for(Response &response) {
+    return std::visit(
+        [](const auto &value) -> InputType {
+          using T = std::decay_t<decltype(value)>;
+          if constexpr (std::is_same_v<T, Content>) {
+            if (value.options.size() > 0)
+              return InputType::CHOICES;
+            return InputType::CONTINUE;
+          } else if constexpr (std::is_same_v<T, Query>) {
+            return InputType::TEXT;
+          } else if constexpr (std::is_same_v<T, Exit>) {
+            return InputType::CONTINUE;
+          } else if constexpr (std::is_same_v<T, GoModule>) {
+            return InputType::CONTINUE;
+          } else if constexpr (std::is_same_v<T, End>) {
+            return InputType::CONTINUE;
+          } else if constexpr (std::is_same_v<T, Error>) {
+            return InputType::CONTINUE;
+          }
+        },
+        response);
+  }
+
+  // Process a response for consumption
+  void process(Response &response) {
+    current_response = response;
+    current_options.clear();
+
+    std::visit(
+        [&](const auto &value) {
+          using T = std::decay_t<decltype(value)>;
+          if constexpr (std::is_same_v<T, Content>) {
+            narrative.push_back(NarrativeItem{.attribution = value.attribution,
+                                              .content = stitch(value.text)});
+            if (value.options.size() > 0) {
+              expected_input = InputType::CHOICES;
+              for (size_t i = 0; i < value.options.size(); i++) {
+                auto &opt = value.options[i];
+                // STUB: Add text for option here
+              }
+            } else {
+              expected_input = InputType::CONTINUE;
+              current_prompt = "...";
+            }
+          } else if constexpr (std::is_same_v<T, Query>) {
+            // STUB: Ask for text
+          } else if constexpr (std::is_same_v<T, Exit>) {
+            // STUB: make a log and continue to exit
+          } else if constexpr (std::is_same_v<T, GoModule>) {
+            // STUB: make a log and continue to jump
+          } else if constexpr (std::is_same_v<T, End>) {
+            // STUB: print out the error
+          } else if constexpr (std::is_same_v<T, Error>) {
+          }
+        },
+        response);
+  }
+
   /** This will stitch a vector of Skald chunks together into one string */
-  std::string stitch(const std::vector<Chunk> &chunks) {
+  static std::string stitch(const std::vector<Chunk> &chunks) {
     std::string ret = "";
     for (auto &chunk : chunks) {
       ret += chunk.text;
@@ -99,77 +177,78 @@ public:
         response);
   }
 
-  Response handle_query(const Query &query) {
-    if (query.expects_response) {
-      std::cout << "\nQUERY: " << query.call.method << " > ";
-    } else {
-      std::cout << "\nCALL: " << query.call.method << " (input anything) > ";
-    }
-    std::string val;
-    std::cin >> val;
+  // Response handle_query(const Query &query) {
+  //   if (query.expects_response) {
+  //     std::cout << "\nQUERY: " << query.call.method << " > ";
+  //   } else {
+  //     std::cout << "\nCALL: " << query.call.method << " (input anything) > ";
+  //   }
+  //   std::string val;
+  //   std::cin >> val;
 
-    std::optional<QueryAnswer> ret = std::nullopt;
-    if (query.expects_response) {
-      SimpleRValue parsed = false;
-      if (val == "true") {
-        parsed = true;
-      } else if (val == "false") {
-        parsed = false;
-      } else {
-        // Try int first (more restrictive than float)
-        char *end;
-        long lval = std::strtol(val.c_str(), &end, 10);
-        if (*end == '\0' && end != val.c_str()) {
-          parsed = static_cast<int>(lval);
-        } else {
-          // Try float
-          float fval = std::strtof(val.c_str(), &end);
-          if (*end == '\0' && end != val.c_str()) {
-            parsed = fval;
-          } else {
-            parsed = val;
-          }
-        }
-      }
-      ret = QueryAnswer{parsed};
-    }
-    return engine.answer(ret);
-  }
+  //   std::optional<QueryAnswer> ret = std::nullopt;
+  //   if (query.expects_response) {
+  //     SimpleRValue parsed = false;
+  //     if (val == "true") {
+  //       parsed = true;
+  //     } else if (val == "false") {
+  //       parsed = false;
+  //     } else {
+  //       // Try int first (more restrictive than float)
+  //       char *end;
+  //       long lval = std::strtol(val.c_str(), &end, 10);
+  //       if (*end == '\0' && end != val.c_str()) {
+  //         parsed = static_cast<int>(lval);
+  //       } else {
+  //         // Try float
+  //         float fval = std::strtof(val.c_str(), &end);
+  //         if (*end == '\0' && end != val.c_str()) {
+  //           parsed = fval;
+  //         } else {
+  //           parsed = val;
+  //         }
+  //       }
+  //     }
+  //     ret = QueryAnswer{parsed};
+  //   }
+  //   return engine.answer(ret);
+  // }
 
-  Response handle_content(const Content &content) {
-    std::cout << stitch(content.text) << "\n";
-    int selected_choice = 0;
-    if (content.options.size() > 0) {
-      std::cout << "\n";
-      for (size_t i = 0; i < content.options.size(); i++) {
-        auto opt = content.options[i];
-        std::cout << (i + 1) << ". " << (opt.is_available ? "" : "<X>")
-                  << stitch(opt.text) << "\n";
-      }
+  // Response handle_content(const Content &content) {
+  //   std::cout << stitch(content.text) << "\n";
+  //   int selected_choice = 0;
+  //   if (content.options.size() > 0) {
+  //     std::cout << "\n";
+  //     for (size_t i = 0; i < content.options.size(); i++) {
+  //       auto opt = content.options[i];
+  //       std::cout << (i + 1) << ". " << (opt.is_available ? "" : "<X>")
+  //                 << stitch(opt.text) << "\n";
+  //     }
 
-      while (true) {
-        std::cout << "\n> ";
-        std::cin >> selected_choice;
-        selected_choice--;
-        if (selected_choice < 0 || selected_choice >= content.options.size()) {
-          std::cout << "Choice out of bounds! Try again.\n";
-          continue;
-        }
-        if (!content.options[selected_choice].is_available) {
-          std::cout << "Choice unavailable! Try again.\n";
-          continue;
-        }
-        break;
-      }
+  //     while (true) {
+  //       std::cout << "\n> ";
+  //       std::cin >> selected_choice;
+  //       selected_choice--;
+  //       if (selected_choice < 0 || selected_choice >= content.options.size())
+  //       {
+  //         std::cout << "Choice out of bounds! Try again.\n";
+  //         continue;
+  //       }
+  //       if (!content.options[selected_choice].is_available) {
+  //         std::cout << "Choice unavailable! Try again.\n";
+  //         continue;
+  //       }
+  //       break;
+  //     }
 
-      return engine.act(selected_choice);
-    } else {
-      int discard;
-      std::cout << "\n (continue) > ";
-      std::cin >> discard;
-      return engine.act(0);
-    }
-  }
+  //     return engine.act(selected_choice);
+  //   } else {
+  //     int discard;
+  //     std::cout << "\n (continue) > ";
+  //     std::cin >> discard;
+  //     return engine.act(0);
+  //   }
+  // }
 
   Engine engine;
 
@@ -177,33 +256,16 @@ public:
   // STUB: Process and return choices
 };
 
-enum InputType { CONTINUE, CHOICES, TEXT };
-
-InputType input_type_for(Response &response) {
-
-  return std::visit(
-      [](const auto &value) -> InputType {
-        using T = std::decay_t<decltype(value)>;
-        if constexpr (std::is_same_v<T, Content>) {
-          return InputType::CONTINUE;
-        } else if constexpr (std::is_same_v<T, Query>) {
-          return InputType::CONTINUE;
-        } else if constexpr (std::is_same_v<T, Exit>) {
-          return InputType::CONTINUE;
-        } else if constexpr (std::is_same_v<T, GoModule>) {
-          return InputType::CONTINUE;
-        } else if constexpr (std::is_same_v<T, End>) {
-          return InputType::CONTINUE;
-        } else if constexpr (std::is_same_v<T, Error>) {
-          return InputType::CONTINUE;
-        }
-      },
-      response);
-}
-
 struct Log {
   std::string attribution = "";
   std::string content = "";
+
+  static Log from_content(Content &content) {
+    return Log{
+        .attribution = content.attribution,
+        .content = SkaldTester::stitch(content.text),
+    };
+  }
 };
 
 int main() {
